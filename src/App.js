4cd49b34e178eb91ba0115a349320aed3da53e54
sungjaecho <liadus@naver.com>
2021-10-12 09:03:49 +0900

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,36 +1,13 @@
-import { useState } from 'react';
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
-  console.log('앱 실행됨.');
+  // 실행시점 : (1) App() 함수가 최초 실행될때
+  useEffect(() => {
+    console.log('useEffect() 실행됨');
+  });
 
-  const [num, setNum] = useState(5);
-
-  let sample = [
-    { id: 1, name: '홍길동' },
-    { id: 2, name: '임꺽정' },
-    { id: 3, name: '장보고' },
-    { id: 4, name: '이순신' },
-  ];
-
-  // 레퍼런스 변경되야 동작함!!
-  const [users, setUsers] = useState(sample);
-
-  const download = () => {
-    const a = sample.concat({ id: num, name: '관창' });
-    setUsers(a);
-    setNum(num + 1);
-  };
-  return (
-    <div>
-      <button onClick={download}>다운로드</button>
-      {users.map((u) => (
-        <h1>
-          {u.id},{u.name}
-        </h1>
-      ))}
-    </div>
-  );
+  return <div>Hello World</div>;
 }
 
 export default App;
