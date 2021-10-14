c45ab850220b751e6727b598f188b939482a085a
sungjaecho <liadus@naver.com>
2021-10-15 08:45:04 +0900

    useEffect 2단계

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  // 실행시점 : (1) App() 함수가 최초 실행될때
+  const [data, setData] = useState(0);
+
+  const download = () => {
+    let downloadData = 5; //가정
+    setData(downloadData);
+  };
+
+  // 실행시점 : (1) App() 함수가 최초 실행될때(App() 그림이 최초 그려질 때)
+  // (2) 상태변수가 변경될 때
   useEffect(() => {
     console.log('useEffect() 실행됨');
-  });
+    download();
+  }, []);
 
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <h1>데이터 : {data}</h1>
+      <button
+        onClick={() => {
+          setData(data + 1);
+        }}
+      >
+        더하기
+      </button>
+    </div>
+  );
 }
 
 export default App;
