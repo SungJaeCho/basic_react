d0b76aa10aaa59c0fd206997e57d5d43b0b47d55
sungjaecho <liadus@naver.com>
2021-10-21 08:49:05 +0900

    yarn설치, styled-compoents설치

--- src/App.js
+++ src/App.js
@@ -1,35 +1,18 @@
-import { createRef, useRef, useState } from 'react';
-
+import styled from 'styled-components';
 import './App.css';
 
-// useRef (디자인)
-// dom을 변경할때 사용
+// document.createElement("h1") 태그와 똑같음.
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
 
 function App() {
-  const myRef = useRef(null);
-
-  const [list, setList] = useState([
-    { id: 1, name: '길동' },
-    { id: 2, name: '꺽정' },
-  ]);
-
-  const myRefs = Array.from({ length: list.length }).map(() => createRef());
-
   return (
     <div>
-      <button
-        onClick={() => {
-          //myRef.current.style.backgroundColor = 'red';
-
-          myRefs[1].current.style.backgroundColor = 'red';
-        }}
-      >
-        색변경
-      </button>
-      <div ref={myRef}>박스</div>
-      {list.map((user, index) => (
-        <h1 ref={myRefs[index]}>{user.name}</h1>
-      ))}
+      <div>안녕</div>
+      <div>헬로우</div>
     </div>
   );
 }
