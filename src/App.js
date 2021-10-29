939542ea7ceea3a97fa3b1de81a2355ccafd4dfc
sungjaecho <liadus@naver.com>
2021-10-29 09:01:24 +0900

    react router dom 설치

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { Route } from 'react-router';
+import Footer from './components/Footer';
+import Header from './components/Header';
 import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Header />
+      <Route path="/" exact={true} component={HomePage} />
+      <Route path="/login" exact={true} component={LoginPage} />
+      <Footer />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root') // document.querySelector('#root')와 같음
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root'), // document.querySelector('#root')와 같음
 );
 
 // If you want to start measuring performance in your app, pass a function
