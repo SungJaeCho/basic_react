a8bbcda867f65f2922080341bfce5e683f9acbc9
sungjaecho <liadus@naver.com>
2021-11-02 09:16:57 +0900

    props 값 출력해보기

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -3,6 +3,8 @@ import Login from '../components/login/Login';
 
 const LoginPage = (props) => {
   console.log('Loginpage', props);
+  console.log(props.match.params.id);
+  props.history.goBack();
   return (
     <div>
       <Login />
