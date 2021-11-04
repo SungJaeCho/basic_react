ce97caf431ebd99583a9c42a47cdd749375a6170
sungjaecho <liadus@naver.com>
2021-11-04 14:55:40 +0900

    부트스트랩 설치

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,24 +5,28 @@ import styled from 'styled-components';
 // 하나의 컴포넌트를 생성 (재사용)
 
 // styled-components => js파일과 css파일 관리
-const StyledHeaderrDiv = styled.div`
+const StyledHeadButtonDiv = styled.div`
   border: 1px solid black;
   height: 300px;
   background-color: ${(props) => props.backgroundColor};
 `;
 
+const StyledHeadLink = styled(Link)`
+  color: red;
+`;
+
 const Header = () => {
   return (
-    <StyledHeaderrDiv backgroundColor="blue">
+    <StyledHeadButtonDiv backgroundColor="blue">
       <ul>
         <li>
-          <Link to="/">홈</Link>
+          <StyledHeadLink to="/">홈</StyledHeadLink>
         </li>
         <li>
-          <Link to="/login/10">로그인</Link>
+          <StyledHeadLink to="/login/10">로그인</StyledHeadLink>
         </li>
       </ul>
-    </StyledHeaderrDiv>
+    </StyledHeadButtonDiv>
   );
 };
 
