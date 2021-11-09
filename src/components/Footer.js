34119b390e40683004af1e0e161c356e629481c1
sungjaecho <liadus@naver.com>
2021-11-09 09:01:11 +0900

    부트스트랩 적용

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,19 +4,19 @@ import styled from 'styled-components';
 // 하나의 컴포넌트를 생성 (재사용)
 
 // styled-components => js파일과 css파일 관리
-const FooterList = styled.div`
+const StyledFooterDiv = styled.div`
   border: 1px solid black;
   height: 300px;
 `;
 
 const Footer = () => {
   return (
-    <FooterList>
+    <StyledFooterDiv>
       <ul>
         <li>오시는길 : 서울시 강남구 V&S빌딩</li>
         <li>전화번호 : 02-1234-4242</li>
       </ul>
-    </FooterList>
+    </StyledFooterDiv>
   );
 };
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,12 @@
 import React from 'react';
+import {
+  Container,
+  Form,
+  FormControl,
+  Nav,
+  Navbar,
+  Button,
+} from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
@@ -17,16 +25,65 @@ const StyledHeadLink = styled(Link)`
 
 const Header = () => {
   return (
-    <StyledHeadButtonDiv backgroundColor="blue">
-      <ul>
-        <li>
-          <StyledHeadLink to="/">홈</StyledHeadLink>
-        </li>
-        <li>
-          <StyledHeadLink to="/login/10">로그인</StyledHeadLink>
-        </li>
-      </ul>
-    </StyledHeadButtonDiv>
+    <div>
+      <StyledHeadButtonDiv backgroundColor="blue">
+        <ul>
+          <li>
+            <StyledHeadLink to="/">홈</StyledHeadLink>
+          </li>
+          <li>
+            <StyledHeadLink to="/login/10">로그인</StyledHeadLink>
+          </li>
+        </ul>
+      </StyledHeadButtonDiv>
+      <>
+        <Navbar bg="dark" variant="dark">
+          <Container>
+            <Navbar.Brand href="#home">다크바</Navbar.Brand>
+            <Nav className="me-auto">
+              <Link to="/" className="nav-link">
+                홈
+              </Link>
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/login">Features</Nav.Link>
+              <Nav.Link href="#pricing">Pricing</Nav.Link>
+            </Nav>
+            <Form className="d-flex">
+              <FormControl
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+          </Container>
+        </Navbar>
+        <br />
+        <Navbar bg="primary" variant="dark">
+          <Container>
+            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#features">Features</Nav.Link>
+              <Nav.Link href="#pricing">Pricing</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+
+        <br />
+        <Navbar bg="light" variant="light">
+          <Container>
+            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#features">Features</Nav.Link>
+              <Nav.Link href="#pricing">Pricing</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+      </>
+    </div>
   );
 };
 
