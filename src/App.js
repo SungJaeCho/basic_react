cc00daf7b620ae2578407ec34d4e37f1757b3283
sungjaecho <liadus@naver.com>
2021-11-10 08:45:03 +0900

    글목록 글쓰기

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+// 글쓰기, 글삭제, 글목록보기
+
 import { Route } from 'react-router';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import HomePage from './pages/HomePage';
-import LoginPage from './pages/LoginPage';
+import Navigation from './components/Navigation';
 
 function App() {
   return (
     <div>
-      <Header />
-      <Route path="/" exact={true} component={HomePage} />
-      <Route path="/login/:id" exact={true} component={LoginPage} />
-      <Footer />
+      <Navigation />
+      <Route path="/" eact={true} component={ListPage} />
+      <Route path="/write" eact={true} component={WritePage} />
     </div>
   );
 }
