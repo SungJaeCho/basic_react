16c5249051d2b56f3019a86380a5bcc7ecb02d2c
sungjaecho <liadus@naver.com>
2021-11-17 08:26:02 +0900

    글쓰기,글목록 실습 (1/2)

--- src/pages/WritePage.js
+++ src/pages/WritePage.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
+const handleWrite = () => {
+  // ListPage의 setPosts에 담기
+  let post = { id: 6, title: '인풋값' };
+};
+
 const WritePage = () => {
   return (
     <div>
       <h1>글쓰기 페이지</h1>
+      <hr />
+      <form>
+        <input type="text" placeholder="제목" />
+        <button type="button" onClick={handleWrite}>
+          글쓰기
+        </button>
+      </form>
     </div>
   );
 };
